6d69dfddc6680baea71becc727e98a03750d5d82
Zachary Weaver <zrweaver447@gmail.com>
2023-08-08 16:53:46 -0400

    feat(sigmoid): added sigmoid function and docs

---
+++ R/math_helpers.R
@@ -0,0 +1,13 @@
+#' Calculates the sigmoid function of the input
+#'
+#' @param float A float number
+#'
+#' @returns A float number which is the result of the sigmoid function
+#'
+#' @examples
+#' sigmoid(0)
+#' sigmoid(2)
+#' 
+sigmoid <- function(float) {
+  return (1.0 / (1 + exp(-float)))
+}
